2af539aab5ce1842373daed2b4ae9fe499dc2d17
Mohdzafar1 <mohdzafar97597@gmail.com>
2025-03-30 23:33:09 +0530

    update frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import DashboardLayout from "./components/ui/DashboardLayout .jsx";
+import { LoginForm } from "./components/auth/LoginForm";
+import RegisterForm from "./components/auth/RegisterForm";
+import ProtectedRoute from "./components/auth/ProtectedRoute"; // Auth Protection
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+      
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+
+      
+        <Route
+          path="/dashboard/*"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }
+        />
+        
+      
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
